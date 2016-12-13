9dd7a71fc177f1003879f60cb26bac62af72a600
Francis <francisjohnpadron@yahoo.com>
2016-12-12 18:49:45 -0700

    update 12-12-2016_02

--- routes/index.js
+++ routes/index.js
@@ -92,15 +92,18 @@ router.post('/sms', function(req, res) {
 
 // Find your account sid and auth token in your Twilio account Console.
 //  var client = twilio('ACaed4ee145e5c490b8c8123895247f81f', 'c08c046c1feebad4d0531054e17442e1');
-var client = twilio('ACb1d4883f47181b77b8e00b2c01a2e33d', '678c16096ad810255d8b5c35ca277fcf');
+  var client = twilio('AC3d81b62658b6dccce7d664f46af118e0', 'ce443214bfe92664c2c6d7a66ca5fccd');
 
   // Send the text message.
   var x = client.sendMessage({
-    // to: '4039185507',
+    
     // to: '+14039185507',
     // from: '+15873338675',
-    from: '+15873174109',
-    to: '4037141510',
+
+    
+    to: '+14037141510',
+    from: '+15873179970',
+
     body: 'Client Name :' + req.body.name + '\nInsurance type :' + req.body.type + '\nClient Phone :' + req.body.phone + '\nClient email :' + req.body.email +  '\nMessage: ' + req.body.message
   
   });
